d169e921c2123555efacac8089fd97652ceee3e3
Janani <jananive@linux.vnet.ibm.com>
2015-02-18 13:10:15 +0530

    libvpd: Terminate the array with '\0' when using strncpy
    If the source length is greater than the destination length,
    terminate the destination with '\0' as strncpy does not append
    a '\0'
    
    Signed-off-by: Janani Venkataraman <jananive@linux.vnet.ibm.com>
    Reviewed-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

--- src/vpddbenv_c.c
+++ src/vpddbenv_c.c
@@ -40,14 +40,18 @@ struct vpddbenv * new_vpddbenv( const char *dir, const char *file )
 	
 	if( dir == NULL || strncmp( dir, "", MAX_NAME_LENGTH ) == 0 )
 		strcpy( ret->envDir, DEFAULT_ENV );
-	else
-		strncpy( ret->envDir, dir, MAX_NAME_LENGTH );
+	else {
+		strncpy( ret->envDir, dir, MAX_NAME_LENGTH - 1);
+		ret->envDir[MAX_NAME_LENGTH - 1] = '\0';
+	}
 	
 	if( file == NULL || strncmp( file, "", MAX_NAME_LENGTH ) == 0 )
 		strcpy( ret->dbFileName, DEFAULT_DB );
-	else
-		strncpy( ret->dbFileName, file, MAX_NAME_LENGTH );
-	
+	else {
+		strncpy( ret->dbFileName, file, MAX_NAME_LENGTH - 1 );
+		ret->dbFileName[MAX_NAME_LENGTH -1] = '\0';
+	}
+
 	strncpy( ret->fullPath, ret->envDir , FULL_PATH_SIZE - 1);
 
 	if (strlen(ret->fullPath) + 1 < FULL_PATH_SIZE)
